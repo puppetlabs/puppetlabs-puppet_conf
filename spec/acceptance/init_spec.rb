d088ee9b54aefd1fb815bf6a99e83cf172c91f27
David Swan <david.swan@puppet.co.uk>
2020-11-10 10:31:30 +0000

    (IAC-1002) - Removal of inappropriate terminology
    The removal of any offensive or inappropriate terminology from the code base.

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -16,15 +16,15 @@ describe 'puppet_conf task' do
     end
 
     it 'set/get a puppet configuration with section' do
-      result = run_bolt_task('puppet_conf', 'action' => 'set', 'setting' => 'storeconfigs', 'value' => 'false', 'section' => 'master')
+      result = run_bolt_task('puppet_conf', 'action' => 'set', 'setting' => 'storeconfigs', 'value' => 'false', 'section' => 'server')
 
       expect(result.exit_code).to eq(0)
-      expect(result['result']).to include 'status' => 'false', 'setting' => 'storeconfigs', 'section' => 'master'
+      expect(result['result']).to include 'status' => 'false', 'setting' => 'storeconfigs', 'section' => 'server'
 
-      result = run_bolt_task('puppet_conf', 'action' => 'get', 'setting' => 'storeconfigs', 'section' => 'master')
+      result = run_bolt_task('puppet_conf', 'action' => 'get', 'setting' => 'storeconfigs', 'section' => 'server')
 
       expect(result.exit_code).to eq(0)
-      expect(result['result']).to include 'status' => 'false', 'setting' => 'storeconfigs', 'section' => 'master'
+      expect(result['result']).to include 'status' => 'false', 'setting' => 'storeconfigs', 'section' => 'server'
     end
   end
 end
