6ec3a51824dc902f10048d262bcf06944545da50
Matthias Fetzer <fetzerms@users.noreply.github.com>
2021-03-06 13:42:32 +0100

    Added tests for delete action

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -4,29 +4,35 @@ require 'spec_helper_acceptance'
 
 describe 'puppet_conf task' do
   describe 'puppet_conf' do
-    it 'set/get a puppet configuration' do
+    it 'set/get/delete a puppet configuration' do
       result = run_bolt_task('puppet_conf', 'action' => 'set', 'setting' => 'vardir', 'value' => '/tmp/bla')
       expect(result.exit_code).to eq(0)
       expect(result['result']['status']).to match %r{.*/tmp/bla}
       expect(result['result']).to include 'setting' => 'vardir', 'section' => 'main'
 
       result = run_bolt_task('puppet_conf', 'action' => 'get', 'setting' => 'vardir')
-
       expect(result.exit_code).to eq(0)
       expect(result['result']['status']).to match %r{.*/tmp/bla}
       expect(result['result']).to include 'setting' => 'vardir', 'section' => 'main'
+
+      result = run_bolt_task('puppet_conf', 'action' => 'delete', 'setting' => 'vardir')
+      expect(result.exit_code).to eq(0)
+      expect(result['result']['status']).to match %r{.*Deleted.*/tmp/bla}
+      expect(result['result']).to include 'setting' => 'vardir', 'section' => 'main'
     end
 
-    it 'set/get a puppet configuration with section' do
+    it 'set/get/delete a puppet configuration with section' do
       result = run_bolt_task('puppet_conf', 'action' => 'set', 'setting' => 'storeconfigs', 'value' => 'false', 'section' => 'server')
-
       expect(result.exit_code).to eq(0)
       expect(result['result']).to include 'status' => 'false', 'setting' => 'storeconfigs', 'section' => 'server'
 
       result = run_bolt_task('puppet_conf', 'action' => 'get', 'setting' => 'storeconfigs', 'section' => 'server')
-
       expect(result.exit_code).to eq(0)
       expect(result['result']).to include 'status' => 'false', 'setting' => 'storeconfigs', 'section' => 'server'
+
+      result = run_bolt_task('puppet_conf', 'action' => 'delete', 'setting' => 'storeconfigs', 'section' => 'server')
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include 'status' => 'Deleted', 'setting' => 'storeconfigs', 'section' => 'server'
     end
   end
 end
